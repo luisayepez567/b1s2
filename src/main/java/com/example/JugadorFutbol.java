0acfe1828d4374f5a72ac40ead9e00a308508074
maryamcita <jukiluma000@gmail.com>
2025-07-15 10:31:09 -0500

    modificacion2

--- src/main/java/com/example/JugadorFutbol.java
+++ src/main/java/com/example/JugadorFutbol.java
@@ -1,5 +1,17 @@
 package com.example;
 
 public class JugadorFutbol {
+    String nombre; 
+    int edad; 
+    String posicion;
+
+    public void presentarse;() {
+        System.out.println("Hola, soy " + nombre + ", tengo " + edad + " años y juego como " + posicion + ".");
+    }
+
+    public void cambiarPosicion (String nuevaPosicion) {
+        posicion = nuevaPosicion;
+        System.out.println(nombre + "ahora juega como" + nuevaPosicion + ".");
+    }
 
 }
--- src/main/java/com/example/Main.java
+++ src/main/java/com/example/Main.java
@@ -8,7 +8,7 @@ public class Main {
         estadio1.nombre = "Estadio Metropolitano";
         estadio1.ciudad = "Barranquilla";
         estadio1.capacidad = 46000;
-        
+
         Estadio estadio2 = new Estadio ();
         estadio2.nombre = "Estadio El Campín";
         estadio2.ciudad = "Bogotá";
@@ -18,6 +18,22 @@ public class Main {
         estadio1.mostrarInformacion();
         estadio2.mostrarInformacion();
 
+        JugadorFutbol jugador1 = new JugadorFutbol();
+        jugador1.nombre = "Carlos";
+        jugador1.edad = 25;
+        jugador1.posicion = "Delantero";
+        jugador1.presentarse();
+        jugador1.cambiarPosicion("Centrocampista");
+        jugador1.presentarse();
+        JugadorFutbol jugador2 = new JugadorFutbol();
+        
+        jugador2.nombre = "Andrés";
+        jugador2.edad = 30;
+        jugador2.posicion = "Defensa";
+        jugador2.presentarse();
+        jugador2.cambiarPosicion("Portero");
+        jugador2.presentarse();
+        
     }
 }
 
