f4a4321b9bbd1301934b62e24499591e228da94e
maryamcita <jukiluma000@gmail.com>
2025-07-15 10:20:47 -0500

    modificacion1

--- src/main/java/com/example/Main.java
+++ src/main/java/com/example/Main.java
@@ -3,7 +3,21 @@ package com.example;
 public class Main {
     public static void main(String[] args) {
 
-       
+
+        Estadio estadio1 = new Estadio ();
+        estadio1.nombre = "Estadio Metropolitano";
+        estadio1.ciudad = "Barranquilla";
+        estadio1.capacidad = 46000;
+        
+        Estadio estadio2 = new Estadio ();
+        estadio2.nombre = "Estadio El Campín";
+        estadio2.ciudad = "Bogotá";
+        estadio2.capacidad = 36000;
+        
+
+        estadio1.mostrarInformacion();
+        estadio2.mostrarInformacion();
+
     }
 }
 
